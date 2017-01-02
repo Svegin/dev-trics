d51b1d6e59f9c4377df31f25dd51feccf2777d67
Svegin <svegin@Svegins-Mini.attlocal.net>
2017-01-02 09:02:50 -0600

    correct js

--- master.js
+++ master.js
@@ -1,6 +1,17 @@
 const dogs = [{name: 'Snickers', age: 3}, {name: 'Barsick', age: 8}];
+
 function makeGreen() {
   const p = document.querySelector('p');
   p.style.color = '#bada55';
   p.style.fontSize= '50 px';
 }
+
+// const buts = document.querySelectorAll('.ink-button');
+// console.log(buts);
+//
+// function addColor(e) {
+//   console.log(e.propertyName);
+//
+// }
+//
+// buts.forEach(buts => buts.addEventListener('click', addColor));
