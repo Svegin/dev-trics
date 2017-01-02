d959976a4d9441a42a940e0dc2d5c1ab84e04c25
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-01 19:36:09 -0600

    add fonts

---
+++ master.js
@@ -0,0 +1,6 @@
+const dogs = [{name: 'Snickers', age: 3}, {name: 'Barsick', age: 8}];
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#bada55';
+  p.style.fontSize= '50 px';
+}
